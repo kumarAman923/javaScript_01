0973d921513d2edcb416a171fe1376de31a9067b
Aman kumar <161278935+kumarAman923@users.noreply.github.com>
2026-02-12 12:01:32 +0000

    done

---
+++ variable_02.js
@@ -0,0 +1,16 @@
+const accountId=12345;
+let username="aman kumar";
+let accountEmail="test@g.com";
+let accountCity="jaipur";
+let accountPassword= 8989;
+console.table([accountId,accountCity,accountEmail,accountPassword,username]);
+
+// accountId=2;
+username="shreya ";
+accountEmail="test99@g.com";
+accountCity="kathgodam";
+accountPassword="0000";
+
+console.table([accountCity,accountId,accountEmail,accountPassword,username]);
+
+
